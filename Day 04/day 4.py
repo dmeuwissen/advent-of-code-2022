4924fd671e598573dc8bd8e67bfc8634e81d9292
dmeuwissen <derekmeuwissen@gmail.com>
2022-12-04 11:46:11 +0100

    Day 4 ugly...

--- Day 04/day 4.py
+++ Day 04/day 4.py
@@ -10,11 +10,11 @@ def get_ranges(string_range):
     return [int(item) for item in string_range.split("-")]
 
 
-def check_if_range_fully_contains_other_range(range_1, range_2):
+def range_fully_contains_other_range(range_1, range_2):
     return range_1[0] <= range_2[0] and range_1[1] >= range_2[1]
 
 
-def check_if_range_partially_contains_other_range(range_1, range_2):
+def range_partially_contains_other_range(range_1, range_2):
     return range_1[0] <= range_2[0] <= range_1[1] or range_1[0] <= range_2[1] <= range_1[1]
 
 
@@ -23,9 +23,9 @@ def part_1(input_list):
     for pair in get_pairs(input_list):
         range1 = get_ranges(pair[0])
         range2 = get_ranges(pair[1])
-        if check_if_range_fully_contains_other_range(range1, range2):
+        if range_fully_contains_other_range(range1, range2):
             overlapping_ranges += 1
-        elif check_if_range_fully_contains_other_range(range2, range1):
+        elif range_fully_contains_other_range(range2, range1):
             overlapping_ranges += 1
     return overlapping_ranges
 
@@ -35,9 +35,9 @@ def part_2(input_list):
     for pair in get_pairs(input_list):
         range1 = get_ranges(pair[0])
         range2 = get_ranges(pair[1])
-        if check_if_range_partially_contains_other_range(range1, range2):
+        if range_partially_contains_other_range(range1, range2):
             overlapping_ranges += 1
-        elif check_if_range_partially_contains_other_range(range2, range1):
+        elif range_partially_contains_other_range(range2, range1):
             overlapping_ranges += 1
     return overlapping_ranges
 
